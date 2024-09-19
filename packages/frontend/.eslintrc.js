60149180ca1681bc62013b15726dde8bca6d47a5
nukeop <12746779+nukeop@users.noreply.github.com>
2024-09-19 23:17:47 +0200

    Frontend eslint config

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -1,34 +1,18 @@
-// const globals = require('globals').browser;
-// const reactHooks = require('eslint-plugin-react-hooks');
-// const reactRefresh = require('eslint-plugin-react-refresh');
-
-// module.exports = {
-//     ignores: [
-//         'dist'
-//     ],
-//     extends: [
-//         "../../.eslintrc.js",
-//     ],
-//     files: ['**/*.{ts,tsx}'],
-//     languageOptions: {
-//         ecmaVersion: 2020,
-//         globals: globals,
-//     },
-//     plugins: {
-//         'react-hooks': reactHooks,
-//         'react-refresh': reactRefresh,
-//     },
-//     rules: {
-//         ...reactHooks.configs.recommended.rules,
-//         'react-refresh/only-export-components': [
-//             'warn',
-//             { allowConstantExport: true },
-//         ]
-//     }
-// };
-
 module.exports = {
     extends: [
         "../../.eslintrc.js",
-    ]
+        "plugin:react-hooks/recommended"
+    ],
+    plugins: [
+        "react-hooks",
+        "react-refresh"
+    ],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+    }
 };
